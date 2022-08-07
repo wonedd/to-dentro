788f0ee35dc7d9780cc0a94b66e1299093a316ba
wonedd <vinmarv@hotmail.com>
2022-08-07 16:07:05 -0300

    initial commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,33 +19,6 @@ export default class MyDocument extends Document {
         return (
             <Html lang="pt-BR">
                 <Head>
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                        href="../assets/apple-touch-icon.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="32x32"
-                        href="../assets/favicon-32x32.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="16x16"
-                        href="../assets/favicon-16x16.png"
-                    />
-                    <link rel="manifest" href="../assets/site.webmanifest" />
-                    <link
-                        rel="preconnect"
-                        href="https://fonts.googleapis.com"
-                    />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;800&display=swap"
-                        rel="stylesheet"
-                    />
                 </Head>
                 <body>
                     <Main />
